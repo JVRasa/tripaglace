2b7f591ce3e3a5822edba63ce7038c90c667937c
JVRasa <j.rasamoelina@gmail.com>
2022-07-21 10:31:31 +0200

    finished home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import iconHome from '../img/iconHome.jpg';
+
 function Home() {
   return (
     <div className="h-screen bg-green p-4">
       <Header />
-      efkjsoijff
+      <div className="h-[calc(100%-80px)] flex flex-col justify-between">
+        <section className="flex flex-col items-center text-center gap-4 mt-28">
+          <img
+            src={iconHome}
+            alt="homepage ice cream"
+            className="w-60 h-60 rounded-full "
+          />
+          <h1 className="text-3xl">
+            Bienvenue
+            <br />
+            sur Le Glacier
+          </h1>
+          <p className="text-xl">Pour les amateurs de bonnes glaces</p>
+        </section>
+        <section className="flex flex-col gap-2">
+          <button
+            type="button"
+            className="bg-dark-blue py-2 rounded-xl font-black"
+          >
+            SE CONNECTER
+          </button>
+          <button type="button">S&rsquo;inscrire</button>
+        </section>
+      </div>
     </div>
   );
 }
--- src/routes/Parlour.jsx
+++ src/routes/Parlour.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Parlour() {
-  return <div>Parlour</div>;
+  return (
+    <div className="h-screen bg-light-blue p-4">
+      {' '}
+      <Header />
+      Parlour
+    </div>
+  );
 }
 
 export default Parlour;
